90b203156323ce7d2905d07551ac3e18a31a2f65
shartsu <shartsu.in@gmail.com>
2015-12-23 00:02:54 +0000

    Add multiple file reading function in particular directory

--- fileProcessor.m
+++ fileProcessor.m
@@ -1,15 +1,35 @@
 
-d = dir('*.csv');
+directory = '1000';
+d = dir(strcat(directory, '/', '*.csv'));
+
+CellArray = zeros(1, 9);
+NumArray = zeros(1, 9);
+DoubleArray = zeros(1, 6);
 
 for n = 1 : numel(d)
     
-    d(n).name
+    fid = fopen(strcat(directory, '/', d(n).name),'r','n','UTF-8');
+    strcat(directory, '/', d(n).name)
     
-    fid = fopen(d(n).name,'r','n','UTF-8');
-    C = textscan(fid,'%s %s %s %d %s %*q %d %d %d %f %d %d %s %d %s %s %*s %s %f %*s %s %s %f %s %*q %*s %s %*q',...
-    'Delimiter',',', 'HeaderLines', 0, 'EmptyValue',-Inf);
-    
-    [Cell, Num, Double] = readCell(C)
+    AllData{n} = textscan(fid,'%s %s %s %d %s %*q %d %d %d %f %d %d %s %d %s %s %*s %s %f %*s %s %s %f %s %*q %*s %s %*q',...
+'Delimiter',',', 'HeaderLines', 0, 'EmptyValue',-Inf);
+
+    [Cell, Num, Double] = readCell(AllData{n});
+
+    if(n == 1)
+        CellArray = Cell;
+        NumArray = Num;
+        DoubleArray = Double;
+    else
+        CellArray = vertcat(CellArray, Cell);
+        NumArray = vertcat(NumArray, Num);
+        DoubleArray = vertcat(DoubleArray, Double);
+    end
     
     fclose(fid);
 end
+
+whos CellArray
+whos NumArray
+whos DoubleArray
+
--- readCell.m
+++ readCell.m
@@ -6,8 +6,11 @@ for i = 1: length(C{2})
     %Split '[Tokyo]' from '4[Tokyo]5' and then put to array D (double->int)
     tmp1 = strsplit(C{2}{i}, {'\D'},'DelimiterType','RegularExpression');
     tmp2 = strsplit(C{2}{i}, {'\d'},'DelimiterType','RegularExpression');
-    N(i, 1) = tmp1(2);
-    R(i, 1) = tmp2(2);
+    
+    if(isempty(cell2mat(tmp2(1))))
+        N(i, 1) = tmp1(2);
+        R(i, 1) = tmp2(2);
+    end
 end
 
 % '[Shiba]2000'  => '[Shiba]' and '2000'
@@ -20,6 +23,8 @@ for i = 1: length(C{14})
     tmp2 = strsplit(C{14}{i},{'\d'},'DelimiterType','RegularExpression');
     E{i, 1}(1) = tmp2{1}(1);
 end
+
+C{19}{1}
 
 %'38.6-36.2' => '38.6' and '36.2'
 for i = 1: length(C{19}) 
@@ -44,15 +49,15 @@ end
 
 Cell = [C{1}, N, R, C{3}, C{5}, C{12}, C{15}, C{18}, C{22}, E];
 %Char
-whos Cell
+%whos Cell
 
 Num = [C{4}, C{6}, C{7}, C{8}, C{10}, C{11}, C{13}, int32(D), H];
 %Num
-whos Num
+%whos Num
 
 Double = [C{9}, C{17}, I, F, G, C{20}];
 %Double
-whos Double
+%whos Double
 %horseData(1).num(:,1)
 
 
